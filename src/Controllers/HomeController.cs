044467752b623a27f274189e3a4680c1ab60a57b
Marwan20036 <51479036+Marwan20036@users.noreply.github.com>
2019-06-07 11:47:18 +0100

    Added project files

---
+++ src/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿namespace MarwanWeb.Controllers
+{
+    using MarwanWeb.Models;
+    using System.Net.Mail;
+    using System.Web.Mvc;
+
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Contact
+            (
+                ContactViewModel model
+            )
+        {
+            var client = new SmtpClient();
+
+            var body = $"From: {model.Email}\nMessage: {model.Message}";
+
+            var message = new MailMessage
+            (
+                "marwan.al.alatoum20036@gmail.com",
+                "marwan.al.alatoum20036@gmail.com",
+                model.Subject,
+                body
+            );
+
+            client.Send(message);
+
+            return View(model);
+        }
+    }
+}
---
+++ src/Models/ContactViewModel.cs
@@ -0,0 +1,37 @@
+﻿namespace MarwanWeb.Models
+{
+    using System;
+    
+    /// <summary>
+    /// Represents a view model for the contact form
+    /// </summary>
+    public class ContactViewModel
+    {
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public string Subject { get; set; }
+
+        public string Message { get; set; }
+
+        public void Validate()
+        {
+            if (String.IsNullOrWhiteSpace(this.Name))
+            {
+                throw new ArgumentException
+                (
+                    "The name is required."
+                );
+            }
+
+            if (String.IsNullOrWhiteSpace(this.Email))
+            {
+                throw new ArgumentException
+                (
+                    "The email address is required."
+                );
+            }
+        }
+    }
+}
